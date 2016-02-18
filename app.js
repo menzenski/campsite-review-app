603806036b8337e3a08398f08da0b512e58b165d
Matt Menzenski <matt.menzenski@gmail.com>
2016-02-18 10:07:10 -0600

    move Campground model to separate file

--- app.js
+++ app.js
@@ -10,13 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-// DB Schema setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
+var Campground = require('./models/campground');
 
 app.get('/', function(request, response) {
     response.render('landing');
